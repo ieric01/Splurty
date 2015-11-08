cf8c535f05017d2375a542a71590b8193e3a3a0c
eric orellana <eric.orellana92@gmail.com>
2015-11-08 01:21:18 +0000

    added test for unique_tag method

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,4 +5,10 @@ class Quote < ActiveRecord::Base
 	validates :author, :presence => true, :length => {
 		:maximum => 50, :minimum => 3
 	}
+	def unique_tag
+		abbr = self.author.split(" ").collect do |sub_string|
+			sub_string[0]
+		end
+		abbr.join + "#" + self.id.to_s
+	end
 end
